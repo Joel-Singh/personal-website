f2df34bd4c51369295d0f77331b2c4f1aabe7dc7
Joel Singh <joelsingh788@gmail.com>
2023-08-26 00:41:43 -0400

    Convert html to pug

--- appUtil/useMiddleware.ts
+++ appUtil/useMiddleware.ts
@@ -4,9 +4,9 @@ import cookieParser from "cookie-parser";
 import logger from "morgan";
 
 export default function useMiddleware(app: Express) {
-    app.use(logger("dev"));
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: false }));
-    app.use(cookieParser());
-    app.use(express.static(path.join(__dirname, "public")));
+  app.use(logger("dev"));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, "public")));
 }
