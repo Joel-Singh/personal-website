0bc61a5a6b88e6df87cb15f6204ce71a93fa75d4
Joel Singh <joelsingh788@gmail.com>
2023-08-26 01:38:54 -0400

    Properly configure build and pug views

--- appUtil/configureViewEngine.ts
+++ appUtil/configureViewEngine.ts
@@ -3,5 +3,5 @@ import path from "path";
 
 export default function configureViewEngine(app: Express) {
   app.set("view engine", "pug");
-  app.set("views", path.join(__dirname, "views"));
+  app.set("views", path.join(__dirname, "../", "views"));
 }
--- appUtil/useMiddleware.ts
+++ appUtil/useMiddleware.ts
@@ -3,10 +3,11 @@ import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
 
+
 export default function useMiddleware(app: Express) {
   app.use(logger("dev"));
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(cookieParser());
-  app.use(express.static(path.join(__dirname, "public")));
+  app.use(express.static(path.join(__dirname, "../", "public")));
 }
