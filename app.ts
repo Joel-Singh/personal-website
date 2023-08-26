e3ce6395644801a92313ab414d35118fd5f1d690
Joel Singh <joelsingh788@gmail.com>
2023-08-26 00:18:27 -0400

    Add pug

--- app.ts
+++ app.ts
@@ -13,6 +13,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
