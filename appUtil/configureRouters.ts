c573b75497dfdeb5f950ca5db464ec77671b3e00
Joel Singh <joelsingh788@gmail.com>
2023-08-31 15:27:23 -0400

    Add about-me route with a simple pug template

--- appUtil/configureRouters.ts
+++ appUtil/configureRouters.ts
@@ -1,6 +1,8 @@
 import { Express } from "express";
 import indexRouter from "../routes/index";
+import aboutMeRouter from "../routes/about-me";
 
 export default function configureRouters(app: Express) {
   app.use("/", indexRouter);
+  app.use("/about-me", aboutMeRouter);
 }
