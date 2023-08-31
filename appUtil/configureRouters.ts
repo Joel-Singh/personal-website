1b86398b4c0be88e04b86114aea0b35d9c8d5b72
Joel Singh <joelsingh788@gmail.com>
2023-08-31 15:23:52 -0400

    Remove unused users route

--- appUtil/configureRouters.ts
+++ appUtil/configureRouters.ts
@@ -1,8 +1,6 @@
 import { Express } from "express";
 import indexRouter from "../routes/index";
-import usersRouter from "../routes/users";
 
 export default function configureRouters(app: Express) {
   app.use("/", indexRouter);
-  app.use("/users", usersRouter);
 }
