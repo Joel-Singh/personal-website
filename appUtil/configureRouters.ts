a6cd2dc27ce5a0f2d7e0efea2239ff0f195c9de9
Joel Singh <joelsingh788@gmail.com>
2023-09-16 21:54:25 -0400

    Add 404 page

--- appUtil/configureRouters.ts
+++ appUtil/configureRouters.ts
@@ -1,8 +1,13 @@
-import { Express } from "express";
+import { Express, RequestHandler } from "express";
 import indexRouter from "../routes/index";
 import aboutMeRouter from "../routes/about-me";
 
 export default function configureRouters(app: Express) {
   app.use("/", indexRouter);
   app.use("/about-me", aboutMeRouter);
+
+  const notFoundPage: RequestHandler = function (req, res, next) {
+    res.status(404).render("404");
+  };
+  app.use(notFoundPage);
 }
