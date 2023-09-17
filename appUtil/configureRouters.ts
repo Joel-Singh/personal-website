c82a7569365e301c1ab34134d6abd0805a1e0832
Joel Singh <joelsingh788@gmail.com>
2023-09-16 22:01:05 -0400

    Add not-done page

--- appUtil/configureRouters.ts
+++ appUtil/configureRouters.ts
@@ -6,6 +6,14 @@ export default function configureRouters(app: Express) {
   app.use("/", indexRouter);
   app.use("/about-me", aboutMeRouter);
 
+  const notDonePage: RequestHandler = (req, res, next) => {
+    res.status(404).render("not-done");
+  };
+
+  app.use("/portfolio", notDonePage);
+  app.use("/contact", notDonePage);
+  app.use("/resume", notDonePage);
+
   const notFoundPage: RequestHandler = function (req, res, next) {
     res.status(404).render("404");
   };
