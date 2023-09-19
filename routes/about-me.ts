922e2277e7bcf7e41a70298a43dd4bf7f6207ea8
Joel Singh <joelsingh788@gmail.com>
2023-09-18 22:20:10 -0400

    Move about-me

--- routes/about-me.ts
+++ routes/about-me.ts
@@ -2,7 +2,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", function (req, res, next) {
-  res.render("about-me");
+  res.render("main-pages/about-me");
 });
 
 export default router;
